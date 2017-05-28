c505902134e56ceb6013898f0c19578c3b01bb92
ClaraBing <liubb16@connect.hku.hk>
2017-05-28 11:55:51 +0800

    clean up staged changes

--- mask.py
+++ mask.py
@@ -5,7 +5,8 @@ import copy
 prefix = 'Mar3/5_900'
 f_ext = '.JPG'
 img_list = ['01', '04', '05', '06', '17', '21', '22', '27']
-# idxes = [1, 4, 5, 6, 17, 21, 22, 27]
+r, g, b = 160, 50, 255
+out_prefix = 'mask-{:d}-{:d}-{:d}/'.format(r,g,b)
 
 for idx in img_list:
   fname = prefix + idx + f_ext
@@ -13,16 +14,14 @@ for idx in img_list:
   img = cv2.imread(fname)
   print(img.shape)
   h,w = img.shape[:2]
-  mask = copy.copy(img)
   
   for i in range(h):
       for j in range(w):
-          if img[i][j][0]<100 and img[i][j][1]<50 and img[i][j][2]<100:
-              mask[i][j][0] = 0
-              mask[i][j][1] = 0
-              mask[i][j][2] = 0
-          else:
-              mask[i][j][0] = 255
-              mask[i][j][1] = 255
-              mask[i][j][2] = 255
-  cv2.imwrite('mask'+str(idx)+'.jpg', mask)
+          if img[i][j][0]>r or img[i][j][1]>g or img[i][j][2]>b:
+              img[i][j][0] = 255
+              img[i][j][1] = 0
+              img[i][j][2] = 0
+  outfile = out_prefix + 'mask'+idx+'.jpg'
+  print(outfile+'\n')
+  cv2.imwrite(outfile, img)
+  break
