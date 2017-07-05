2633dae6395753acbcbc5ec47a2226eb70990c1d
ClaraBing <liubb16@connect.hku.hk>
2017-07-05 14:32:35 +0800

    sift: add flag for visualization

--- traditional/random_forest/sift.py
+++ traditional/random_forest/sift.py
@@ -7,6 +7,6 @@ gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 sift = cv2.xfeatures2d.SIFT_create()
 kp = sift.detect(gray,None)
 
-cv2.drawKeypoints(gray,kp, img)
+cv2.drawKeypoints(gray,kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 cv2.imwrite('sift_lacto000000.jpg',img)
