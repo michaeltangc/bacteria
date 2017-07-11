6b15ee3a5da23a885dcb286cba71fbb491bfbae7
Ubuntu <ubuntu@ct014814.lxd>
2017-07-11 11:23:41 +0800

    test counting on bacte

---
+++ cnn/config_count_7.lua
@@ -0,0 +1,33 @@
+local cfg = {
+    -- image
+    class_count = 10, -- count 0-9
+    normalization = {method = 'contrastive', width=7, centering=true, scaling=false},
+    augmentation = {vflip=0.25, hflip=0.25, random_scaling=0, aspect_jitter=0},
+    color_space = 'rgb',
+    batch_size = 16,
+    pos_thresh = 0.7,
+    neg_thresh = 0.25,
+    -- training
+    -- train_img_set = '../data/train',
+    -- test_img_set = '../data/test',
+    -- cache = './cache/',
+}
+
+local opt = {
+    -- Train
+    model = 'model_conv7pool7.lua',
+    ftrain = '../data/db_square_224_val01_bacte.t7',
+    restore_train = '',
+    restore_test = '', -- 'conv7pool7_context_bn/conv7pool7_004000.t7',
+    enumerate_models = true,
+    lr = 1e-3,
+    eps = 1e-7, -- parameter for batch normalization
+    snapshot = 1000,
+    snapshot_prefix = 'conv7pool7_white_bg_bacte/conv7pool7',
+    plot = 200,
+    gpuid = 0, -- Note: only 1 GPU available on this virtual machine
+    -- Test
+    result_dir = 'conv7pool7_white_bg_bacte/', -- Note: always add a trailing '/'
+}
+
+return cfg, opt
