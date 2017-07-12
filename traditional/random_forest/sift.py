682bd880da143672813ac73afb4f9b8f9b17daf4
ClaraBing <liubb16@connect.hku.hk>
2017-07-12 11:44:26 +0800

    SIFT update

--- traditional/random_forest/sift.py
+++ traditional/random_forest/sift.py
@@ -1,12 +1,25 @@
 import cv2
 import numpy as np
+from sklearn.cluster import KMeans
+from glob import glob
 
-img = cv2.imread('square_lacto000000.jpg')
-gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+desc_all = np.empty((0,128))
+for each in glob('square_lacto*.jpg'):
+    img = cv2.imread(each)
+    gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-sift = cv2.xfeatures2d.SIFT_create()
-kp = sift.detect(gray,None)
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp, desc = sift.detectAndCompute(gray,None)
+    desc_all = np.concatenate((desc_all, desc))
 
-cv2.drawKeypoints(gray,kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+#     white = np.copy(img)
+#     white.fill(1)
+#     cv2.drawKeypoints(gray,kp, white, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# 
+#     cv2.imwrite(each.replace('square', 'sift_white'), white)
 
-cv2.imwrite('sift_lacto000000.jpg',img)
+print('desc_all.shape:')
+print(desc_all.shape)
+
+kmeans = KMeans(n_clusters=100, random_state=0).fit(X)
+
