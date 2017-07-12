96a2fc42a6dd5308ffbac45d8b43d0d8fbc32fac
ClaraBing <liubb16@connect.hku.hk>
2017-07-12 11:41:13 +0800

    setPass2Only s.t. only product dark regions (i.e. bacteria)

--- traditional/mser/mser_gen_sample.py
+++ traditional/mser/mser_gen_sample.py
@@ -2,17 +2,16 @@ import cv2
 import time
 
 t = time.clock()
+mser = cv2.MSER_create()
+mser.setPass2Only(True) # Pass2Only: only use the inverted image for detecting MSER
 for i in range(1,32):
-    img = cv2.imread('Mar2/5_900{:02d}.JPG'.format(i))
-    mser = cv2.MSER_create()
-    gray_img = img.copy()
-    gray = cv2.cvtColor(gray_img, cv2.COLOR_BGR2GRAY)
-    regions = mser.detectRegions(gray, None)
-    print(len(regions))
-    print(len(regions[0]))
-    print(regions[0][0])
+    img = cv2.imread('/home/bingbin/bacteria/data/Mar2/5_900{:02d}.JPG'.format(i))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    white = gray.copy()
+    white.fill(255)
+    gray_inverted = white - gray # Invert the image s.t. Pass2Only will give the dark regions as desired
+    regions = mser.detectRegions(gray_inverted, None)
     hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
     cv2.polylines(img, hulls, 1, (0,255,0), 2)
-    cv2.imwrite('mser/mser{:02d}.jpg'.format(i), img)
-    break
+    cv2.imwrite('pass2_only/inverted_mser{:02d}.jpg'.format(i), img)
 print((time.clock() - t) / 31)
