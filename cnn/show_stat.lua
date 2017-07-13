8220a60a9ff791a89767ef5e51cb297bd389bd91
ClaraBing <liubb16@connect.hku.hk>
2017-07-13 17:50:25 +0800

    clean up folder

--- cnn/show_stat.lua
+++ cnn/show_stat.lua
@@ -1,18 +1,22 @@
+-- Plot training progress of a model of choice.
+-- The progress is averaged over a fixed number (i.g. the variable named 'step') of iterations
+
 require 'util'
 
 local cfg, opt = dofile('config.lua')
-dir = 'conv5pool5_white_bg/'
-restored = dir .. 'conv5pool5.t7'
-model_factory = 'model_conv5pool5.lua'
+dir = '' -- the directory where your model is placed; ALWAYS add a trailing '/'
+restored = dir .. '' -- filename of your model (*.t7)
+model_factory = '' -- filename of your model factory file, e.g. model_*.lua
 _, _, _, training_stats = load_model(cfg, opt, model_factory, restored)
-print(training_stats)
+
 local avg_stats = {}
-for i=1,#training_stats.loss,10 do
+step = 10
+for i=1,#training_stats.loss,step do
     local curr_sum = 0
-    for j=0,9 do
+    for j=0,step-1 do
         curr_sum = curr_sum + training_stats.loss[i+j]
     end
-    curr_sum = curr_sum / 10
+    curr_sum = curr_sum / step
     table.insert(avg_stats, curr_sum)
 end
 plot_stat(dir .. 'avg_stat', avg_stats)
